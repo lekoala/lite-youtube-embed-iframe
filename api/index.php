cdb194e7aa9a9931d525da640bd1f05c096c2280
Thomas <thomas@lekoala.be>
2021-09-07 12:01:58 +0200

    init

---
+++ api/index.php
@@ -0,0 +1,38 @@
+<?php
+
+$title = $_GET['title'] ?? '';
+$videoid = $_GET['video_id'] ?? '';
+$params = $_GET['params'] ?? '';
+$player = "";
+if ($videoid) {
+    $player = <<<PLAYER
+<lite-youtube videoid="$videoid" params="$params" style="background-image: url('https://i.ytimg.com/vi/$videoid/hqdefault.jpg">
+<button type="button" class="lty-playbtn">
+    <span class="lyt-visually-hidden">$title</span>
+</button>
+</lite-youtube>
+PLAYER;
+}
+
+$html = <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$title</title>
+    <link rel="stylesheet" href="node_modules/lite-youtube-embed/src/lite-yt-embed.css" />
+    <script src="node_modules/lite-youtube-embed/src/lite-yt-embed.js"></script>
+    <style type="text/css">
+    *{padding:0;margin:0;overflow:hidden}
+    html,body{height:100%}
+    </style>
+</head>
+<body>
+$player
+</body>
+</html>
+HTML;
+
+echo $html;
